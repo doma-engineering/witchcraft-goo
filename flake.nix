53a215cbbde690d879c8da17ed11b188cff5f252
cognivore <66186054+cognivore@users.noreply.github.com>
2023-02-22 02:40:51 +0000

    Problem: No way to use Goo as elixir toolchain in dev
    Solution:
    
     - Add nix flake, which fetches `goo` from github

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs";
+        goo.url = "github:doma-engineering/goo";
+    };
+
+    outputs = {self, nixpkgs, goo}:
+        let pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        in {
+            defaultPackage.x86_64-linux = pkgs.hello;
+
+            devShell.x86_64-linux =
+                pkgs.mkShell {
+                    buildInputs = [
+                        pkgs.erlang
+                        goo.defaultPackage.x86_64-linux
+                    ];
+                };
+        };
+}
--- mix.exs
+++ mix.exs
@@ -25,28 +25,27 @@ defmodule Witchcraft.Mixfile do
   defp aliases do
     [
       quality: [
-          "test",
-          "credo --strict"
-        ]
+        "test",
+        "credo --strict"
+      ]
     ]
   end
 
   defp deps do
     [
-      {:benchfella,  "~> 0.3",  only: [:dev, :test]},
-      {:credo,       "~> 1.5",  only: [:dev, :test],        runtime: false},
-      {:inch_ex,     "~> 2.0",  only: [:dev, :docs, :test], runtime: false},
-
-      {:dialyxir,    "~> 1.1",  only: :dev,                 runtime: false},
-      {:earmark,     "~> 1.4",  only: :dev,                 runtime: false},
-      {:ex_doc,      "~> 0.23", only: :dev,                 runtime: false},
-
+      {:benchfella, "~> 0.3", only: [:dev, :test]},
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
+      {:inch_ex, "~> 2.0", only: [:dev, :docs, :test], runtime: false},
+      {:dialyxir, "~> 1.1", only: :dev, runtime: false},
+      {:earmark, "~> 1.4", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.23", only: :dev, runtime: false},
       {:exceptional, "~> 2.1"},
-
-      {:operator,    "~> 0.2"},
-
-      {:quark,       "~> 2.2"},
-      {:type_class,  "~> 1.2"}
+      {:operator, "~> 0.2"},
+      {:quark, "~> 2.2"},
+      # {:type_class,  "~> 1.2"}
+      ## Get type_class from ~/github/type_class-goo/
+      # {:type_class_goo, path: "~/github/type_class-goo"},
+      {:type_class_goo, "~> 1.21"}
     ]
   end
 
